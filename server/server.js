a762e48ee327b4c039ea4567d3aa811a082601aa
Ahmed Hatem <ahmedhatemomran@gmail.com>
2025-12-29 14:10:33 +0200

    fix: import stripe webjooks

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const port = 3000;
 await connectDB();
 
 //Sripe webhook route
-app.use("/api/stripe", express.raw({type: "application/json"}, stripeWebhooks));
+app.use("/api/stripe", express.raw({type: "application/json"}), stripeWebhooks);
 
 //Middleware
 app.use(express.json());
